8d9c9f23d20869d808dd7659cd2104d6e300ead7
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-19 10:52:56 +0700

    modify quiz methods to return string for the ui

--- brain.py
+++ brain.py
@@ -11,24 +11,13 @@ class Quiz:
         self.max_number = len(self.questions) - 1
 
     def check_answer(self, answer):
-        if answer[0] == self.questions[self.number].answer[0]:
-            print("You got it right!")
+        if answer == self.questions[self.number].answer:
+            self.number += 1
             self.score += 1
-        else:
-            print("You're wrong")
-        self.check_info()
-
-    def check_info(self):
-        print(f"The correct answer was {self.questions[self.number].answer}")
-        print(f"Your current score is: {self.score}/{self.max_number}\n")
+            return True
         self.number += 1
-        if self.number > self.max_number:
-            print("You've completed the quiz!")
-            print(f"Your final score was: {self.score}/{self.max_number}")
-            return
-        self.next_question()
 
     def next_question(self):
-        user_ans = input(f"Q.{self.number}: {html.unescape(self.questions[self.number].question)} "
-                         f"(True/False)?: ").capitalize()
-        self.check_answer(user_ans)
+        if self.number > self.max_number:
+            return False, False
+        return f"Q.{self.number}", f"{html.unescape(self.questions[self.number].question)}"
